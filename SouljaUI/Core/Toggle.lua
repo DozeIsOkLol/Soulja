cba2b2c162505b85599158c9cb470388c897dd88
DOZE / UILib <afterlife.records@outlook.com>
2025-12-23 19:08:29 +0000

    Update Toggle.lua

--- SouljaUI/Core/Toggle.lua
+++ SouljaUI/Core/Toggle.lua
@@ -1,1 +1,8 @@
-
+-- /Core/Toggle.lua
+return function(SouljaUI)
+    return function()
+        if SouljaUI.Properties.ScreenGui then
+            SouljaUI.Properties.ScreenGui.Enabled = not SouljaUI.Properties.ScreenGui.Enabled
+        end
+    end
+end
