39fcff34363744a6347fd962598df9f118cf43c5
DOZE / UILib <afterlife.records@outlook.com>
2025-12-23 19:12:11 +0000

    Update Label.lua

--- SouljaUI/Elements/Label.lua
+++ SouljaUI/Elements/Label.lua
@@ -1,1 +1,12 @@
-
+-- /Elements/Label.lua
+return function(parent, TextService, UserInputService, text) 
+    local Label = Instance.new("TextLabel", parent); 
+    Label.BackgroundTransparency = 1; 
+    Label.Size = UDim2.new(1, 0, 0, 20); 
+    Label.Font = Enum.Font.GothamSemibold; 
+    Label.Text = text; 
+    Label.TextColor3 = Color3.fromRGB(255, 255, 255); 
+    Label.TextSize = 16; 
+    Label.TextXAlignment = Enum.TextXAlignment.Left; 
+    return {} 
+end
